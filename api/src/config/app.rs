fb60be2757c22b666c93422a33bbd7f1a692cded
laynath <laynath2018@gmail.com>
2020-11-03 17:03:48 +0700

    add rest api

---
+++ api/src/config/app.rs
@@ -0,0 +1,16 @@
+use crate::api::*;
+use actix_web::web;
+
+pub fn config_services(cfg: &mut web::ServiceConfig) {
+    info!("Configurating routes...");
+    cfg.service(
+        web::scope("/api").service(
+            web::scope("/auth")
+                .service(web::resource("/signup").route(web::post().to(account_controller::signup)))
+                .service(web::resource("/login").route(web::post().to(account_controller::login)))
+                .service(
+                    web::resource("/logout").route(web::post().to(account_controller::logout)),
+                ),
+        ),
+    );
+}
---
+++ api/src/models/mod.rs
@@ -0,0 +1,4 @@
+pub mod login_history;
+pub mod response;
+pub mod user;
+pub mod user_token;
