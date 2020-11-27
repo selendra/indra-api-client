5ca0b061374336aa85af76745049af00b8ac0213
LayNath242 <laynath2018@gmail.com>
2020-11-27 15:10:58 +0700

    init

--- script/init.sh
+++ script/init.sh
@@ -23,9 +23,9 @@ else
     export PATH=$HOME/.cargo/bin:$PATH
 fi
 
-rustup install nightly-2020-07-02
-rustup override set nightly-2020-07-02
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-07-02
+rustup install nightly-2020-10-06
+rustup override set nightly-2020-10-06
+rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06
 
 if [[ $(wasm-gc) ]]; then
     echo "Found wasm-gc"
